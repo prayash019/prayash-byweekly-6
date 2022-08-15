6ebb75318318f619b9ff195a6620f4226d0b351e
prayash019 <110799850+prayash019@users.noreply.github.com>
2022-08-15 15:40:01 +1000

    update 1

--- student.cs
+++ student.cs
@@ -8,9 +8,9 @@ namespace WebApplication1.Models
     public class Student
     {
         public int studentId { get; set; }
-        public string studentName { get; set; }
+        public string teachertName { get; set; }
         public string studentNationality { get; set; }
-        public int studentAge { get; set; }
+        public int teacherAge { get; set; }
 
     }
 }
